d94fb741e91cc81b88bd92e951a3de75988933a3
Tim Goshinski <tim@code-chimp.com>
2022-06-17 09:05:35 -0500

    :sparkles: personal changes to base project

--- web-components-storyshots.test.js
+++ web-components-storyshots.test.js
@@ -9,8 +9,8 @@ import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 const getMatchOptions = ({ context: { fileName } }) => {
   // Generates a custom path based on the file name and the custom directory.
   const snapshotPath = join(
-    'packages',
-    'component',
+    // 'packages',
+    // 'component',
     dirname(fileName),
     '__snapshots__'
   );
