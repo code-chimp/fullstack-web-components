893df12aa10d91384a7db6b9b5d56e7b9d97d1d6
Tim Goshinski <tim@code-chimp.com>
2022-06-17 09:22:50 -0500

    :sparkles: Card component

---
+++ packages/component/src/card/Card.ts
@@ -0,0 +1,45 @@
+export class CardComponent extends HTMLElement {
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          background: var(--color-white);
+          border-radius: var(--radius-md);
+          box-shadow: var(--shadow);
+          overflow: hidden;
+          max-width: 320px;
+        }
+        
+        ::slotted(*) {
+          padding-left: var(--padding-lg);
+          padding-right: var(--padding-lg);
+        }
+      
+        ::slotted(a) {
+          display: block;
+        }
+        
+        ::slotted(img) {
+          padding-left: 0;
+          padding-right: 0;
+          width: 100%;
+        }
+        
+        ::slotted(:last-child) {
+          padding-bottom: var(--margin-lg);
+        }
+      </style>
+      <header><slot name="header"></slot></header>
+      <section><slot name="content"></slot></section>
+      <footer><slot name="footer"></slot></footer>`;
+
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('in-card', CardComponent);
